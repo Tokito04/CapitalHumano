d1ab1789dc86f12cacc5d4b8c599d51b31a0741f
Charlie <cechevers0620@gmail.com>
2025-07-15 09:41:59 -0500

    Definida la estructura del proyecto
    Añadidos también archivos de relleno para que se actualice la estructura

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+// Incluimos el autoloader de Composer
+require_once __DIR__ . '/../vendor/autoload.php';
+
+echo "¡Proyecto 'Capital Humano' configurado!";
+
+// Aquí irá nuestro código de enrutamiento más adelante
